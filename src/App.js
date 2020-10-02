a5386e1875e49a8d9463e3f15d3164cfc6a42e2a
= <=>
2020-10-02 17:54:30 +0200

    021020:1754

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Sidebar from './Sidebar'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> LEARN REACT
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-      </header>
+    <div className="app">
+      <h1>
+        HOLA
+         
+        < Sidebar />
+
+        {/* Chat */}
+
+      </h1>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD1N7cxZ32enBAwC4LdyDEaO-9Bf-XZiK0",
+    authDomain: "whatsapp-3b973.firebaseapp.com",
+    databaseURL: "https://whatsapp-3b973.firebaseio.com",
+    projectId: "whatsapp-3b973",
+    storageBucket: "whatsapp-3b973.appspot.com",
+    messagingSenderId: "973990014821",
+    appId: "1:973990014821:web:a49cf58dee777553bb3b8c"
+  };
