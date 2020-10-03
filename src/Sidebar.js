9d1656abc4383903baa7e6fe93440c226ec04d98
= <=>
2020-10-03 12:20:47 +0200

    031020:1220

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,13 +4,15 @@ import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import {Avatar,IconButton} from  '@material-ui/core';
 import  MoreVertIcon  from '@material-ui/icons/MoreVert';
 import ChatIcon from '@material-ui/icons/Chat';
+import {SearchOutlined} from '@material-ui/icons';
 
 function Sidebar() {
     return (
         <div className="sidebar">
             
-
             <div className="sidebar_header">
+
+                <Avatar src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
 
                 <div className="sidebar_headerRight">
 
@@ -38,6 +40,22 @@ function Sidebar() {
                 </div>
 
             </div>
+
+            <div className="sidebar_search">
+
+                <div className="sidebar_searchContainer">
+
+                    <SearchOutlined />
+
+                    <input placeholder="Search o start new chat" type="text" />
+
+                </div>
+
+
+            </div>
+
+
+            
         </div>
     )
 }
