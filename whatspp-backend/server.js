80dfc723d21e9727b87bea24c16b0e79b6354bdf
= <=>
2020-10-05 17:49:28 +0200

    051020:1749

--- whatspp-backend/server.js
+++ whatspp-backend/server.js
@@ -1,9 +1,22 @@
 import express from 'express'
-import mongoose from 'mongoose'
-import Messages from './dbMessages.js'
+import mongoose from 'mongoose';
+import Messages from './dbMessages.js';
+import Pusher from 'pusher';
 
 const app = express()
 const port = process.env.PORT || 9000
+
+const pusher = new Pusher({
+    appId: '1085205',
+    key: '17ad1317c79d34f57935',
+    secret: '3b6cf53f97c9de1af100',
+    cluster: 'eu',
+    encrypted: true
+  });
+
+
+app.use(express.json());
+
 
 const connection_url = 'mongodb+srv://admin:sobrino1@cluster0.brqvj.mongodb.net/whatsapp?retryWrites=true&w=majority'
 
@@ -16,6 +29,20 @@ mongoose.connect(connection_url, {
 
 });
 
+const db = mongoose.connection
+
+db.once('open', ()=>{
+
+    console.log("DB CONNECTED");
+    const msgCollection = db.collection("messagecontents");
+    const changeStream = msgCollection.watch();
+
+    changeStream.on('change', (change)=>{
+
+        console.log(change);
+
+    });
+});
 
 
 app.get('/',(req,res)=>res.status(200).send("HELLO"));
@@ -38,6 +65,20 @@ app.post('/messages/new', (req,res)=>{
     });
 
 
+});
+
+app.get('/messages/sync', (req,res)=>{
+
+    Messages.find((err, data) =>{
+
+        if (err){
+            res.status(500).send(err)
+        }else {
+
+            res.status(200).send(data)
+        }
+
+    });
 });
 
 app.listen(port,()=>console.log(`Running ${port}`));
